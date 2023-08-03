945da90af9bc88ea3f2b302da0dc84a1f7a4f1b8
Selvadharshini96 <116495927+Selvadharshini96@users.noreply.github.com>
2023-08-03 09:57:47 +0530

    Add files via upload

---
+++ project5/prj/prj1/src/main/java/com/example/QueryAnnotation/QueryAnnotationpplication.java
@@ -0,0 +1,13 @@
+package com.example.QueryAnnotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryAnnotationpplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryAnnotationpplication.class, args);
+	}
+
+}
